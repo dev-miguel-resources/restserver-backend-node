d0c3575a37e47815d2a99346bfee878e6869d724
dev-miguel-resources <ingenieromiguelch@gmail.com>
2020-10-14 00:56:42 -0300

    aplicando ejemplos de documentación con swagger

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,42 @@ const { userSignupValidator } = require("../validator");
 
 // routes
 router.post("/signup", userSignupValidator, signup);
+
+/**
+ * @swagger   
+ * /api/signin: 
+ *  post:
+ *    summary: signin user
+ *    description: Use to request signin user
+ *    requestBody: 
+ *      content:
+ *        application/json:
+ *          schema:
+ *            properties:
+ *              email:
+ *                  type: string
+ *                  description: email user valid
+ *              password:
+ *                  type: string
+ *                  description: password user valid
+ *    responses:
+ *      "200":
+ *         description: A successful response
+ *      "400":
+ *         description: A bad request response
+ */
 router.post("/signin", signin);
+
+/**
+ * @swagger
+ * /api/signout:
+ *  get:
+ *    summary: signout user
+ *    description: Use to request logout user
+ *    responses:
+ *      "200":
+ *        description: A successful response
+ */
 router.get("/signout", signout);
 
 module.exports = router;
